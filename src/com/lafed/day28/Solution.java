814a70aaffd6df69a64431e7f5e0ce0160305cd6
iamrdb2f <dev@rolandguissony.com>
2025-04-11 20:05:42 +0200

    Test case succeeded

--- src/com/lafed/day28/Solution.java
+++ src/com/lafed/day28/Solution.java
@@ -1,4 +1,50 @@
 package com.lafed.day28;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> gmailUsers = new ArrayList<>();
+        Pattern gmailPattern = Pattern.compile(".+@gmail\\.com$");
+
+        IntStream.range(0, N).forEach(NItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                String firstName = firstMultipleInput[0];
+
+                String emailID = firstMultipleInput[1];
+
+                if (gmailPattern.matcher(emailID).matches()) {
+                    gmailUsers.add(firstName);
+                }
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+
+        Collections.sort(gmailUsers);
+
+        for (String firstName : gmailUsers) {
+            System.out.println(firstName);
+        }
+    }
 }
